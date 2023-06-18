b15d0126f031694e7c1b075e9ca56eb5a0aff7fa
deekshithtirumala <105743252+deekshithtirumala@users.noreply.github.com>
2023-06-18 21:48:45 +0530

    Add files via upload

--- TIC TAC TOE.py
+++ TIC TAC TOE.py
@@ -1,293 +1,94 @@
 from turtle import *
-win=Screen()
-j=0#person name if 0 for 'x' and 1 for 'o'
-win.colormode(255)
-win.bgcolor((20,189,172))
-bgdrawer=Turtle(visible=False)# writer of 'x' or 'o'
-bgdrawer.pensize(10)
-bgdrawer.pencolor('grey')
-bgdrawer.up()
-win.tracer(0)
-bgdraw=Turtle(visible=False)#lines drawing
-bgdraw.pensize(10)
-bgdraw.pencolor((83,83,83))
-notify=Turtle(visible=False)#the text you see down
-notify.up()
-notify.goto(0,-180)
 
-#names of plces
-a1=0
-a2=0
-a3=0
-a4=0
-a5=0
-a6=0
-a7=0
-a8=0
-a9=0
-def ngame():#new game
-    global a1,a2,a3,a4,a5,a6,a7,a8,a9
-    bgdrawer.clear()
-    notify.clear()
-    a1=0
-    a2=0
-    a3=0
-    a4=0
-    a5=0
-    a6=0
-    a7=0
-    a8=0
-    a9=0
-    notify.write('Ok'.format(a1),move=False, align="center", font=("Arial", 18, "normal"))
-    initalizer()
-def initalizer():
-    global j
-    if j==0:
-        listen()
-        win.onclick(anlyser)
-        j=1
-    elif j==1:
-        listen()
-        win.onclick(anlyser)
-        j=0
-def chek():#to veryify there is a winner our not
-    global a1,a2,a3,a4,a5,a6,a7,a8,a9
-    if ((a1==a2 and a2==a3)or(a1==a4 and a4==a7)or(a1==a5 and a5==a9))and(a1!=0):
-        notify.clear()
-        notify.write('{} is the winner and press space for start'.format(a1),move=False, align="center", font=("Arial", 18, "normal"))
-        listen()
-        onkey(ngame,'space')
-        win.onclick(None)
-    elif (a4==a5 and a5==a6)and(a4!=0):
-        notify.clear()
-        notify.write('{} is the winner and press space for start'.format(a4),move=False, align="center", font=("Arial", 18, "normal"))
-        listen()
-        onkey(ngame,'space')
-        win.onclick(None)
-    elif ((a7==a8 and a8==a9)or(a7==a5 and a5==a3))and(a7!=0):
-        notify.clear()
-        notify.write('{} is the winner and press space for start'.format(a7),move=False, align="center", font=("Arial", 18, "normal"))
-        listen()
-        win.onclick(None)
-        onkey(ngame,'space')
-    elif (a2==a5 and a5==a8)and(a2!=0):
-        notify.clear()
-        notify.write('{} is the winner and press space for start'.format(a2),move=False, align="center", font=("Arial", 18, "normal"))
-        listen()
-        onkey(ngame,'space')
-        win.onclick(None)
-    elif (a3==a6 and a6==a9)and(a3!=0):
-        notify.clear()
-        notify.write('{} is the winner and press space for start'.format(a3),move=False, align="center", font=("Arial", 18, "normal"))
-        listen()
-        onkey(ngame,'space')
-        win.onclick(None)
-    elif (a1!=0 and a2!=0 and a3!=0 and a4!=0 and a5!=0 and a6!=0 and a7!=0 and a8!=0 and a9!=0):
-        ngame()
-    else:
-        initalizer()
-def bg():#fuction to draw lines
-    bgdraw.up()
-    bgdraw.goto(-150,50)
-    bgdraw.down()
-    bgdraw.goto(150,50)
-    bgdraw.up()
-    bgdraw.goto(-150,-50)
-    bgdraw.down()
-    bgdraw.goto(150,-50)
-    bgdraw.up()
-    bgdraw.goto(-50,150)
-    bgdraw.down()
-    bgdraw.goto(-50,-150)
-    bgdraw.up()
-    bgdraw.goto(50,150)
-    bgdraw.down()
-    bgdraw.goto(50,-150)
-    bgdraw.up()
-    win.tracer(0)
-def score(x,y):#changing value
-    global j,a1,a2,a3,a4,a5,a6,a7,a8,a9
-    if j==0:
-        bgdrawer.pencolor('grey')
-        if x==1:#x=2 o=1
-            a1='X'
-        elif x==2:
-            a2='X'
-        elif x==3:
-            a3='X'
-        elif x==4:
-            a4='X'
-        elif x==5:
-            a5='X'
-        elif x==6:
-            a6='X'
-        elif x==7:
-            a7='X'
-        elif x==8:
-            a8='X'
-        elif x==9:
-            a9='X'
-        chek()
-    elif j==1:
-        bgdrawer.pencolor('White')
-        if x==1:#x=2 o=1
-            a1='O'
-        elif x==2:
-            a2='O'
-        elif x==3:
-            a3='O'
-        elif x==4:
-            a4='O'
-        elif x==5:
-            a5='O'
-        elif x==6:
-            a6='O'
-        elif x==7:
-            a7='O'
-        elif x==8:
-            a8='O'
-        elif x==9:
-            a9='O'
-        chek()
-def draw(a):#going to that positon  and writing x and o 
-    global j,a1,a2,a3,a4,a5,a6,a7,a8,a9
-    if a==1:
-    	if a1==0:
-	        bgdrawer.goto(-100,46)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==2:
-    	if a2==0:
-	        bgdrawer.goto(0,46)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==3:
-    	if a3==0:
-	        bgdrawer.goto(100,46)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==4:
-    	if a4==0:
-	        bgdrawer.goto(-100,-56)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==5:
-    	if a5==0:
-	        bgdrawer.goto(0,-56)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==6:
-    	if a6==0:
-	        bgdrawer.goto(100,-56)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==7:
-    	if a7==0:
-	        bgdrawer.goto(-100,-156)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==8:
-    	if a8==0:
-	        bgdrawer.goto(0,-156)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-    elif a==9:
-    	if a9==0:
-	        bgdrawer.goto(100,-156)
-	        if j==0:
-	            bgdrawer.write('X',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'x')
-	            j=1
-	        elif j==1:
-	            bgdrawer.write('O',move=False, align="center", font=("Arial", 72, "normal"))
-	            score(a,'o')
-	            j=0
-def anlyser(x,y):# know where the player tapped
-    if ((x>-150 and x<-50)and(y<150 and y>50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(1)
-    elif ((x>-50 and x<50)and(y<150 and y>50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(2)
-    elif ((x>50 and x<150)and(y<150 and y>50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(3)
-    elif ((x>-150 and x<-50)and(y<50 and y>-50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(4)
-    elif ((x>-50 and x<50)and(y<50 and y>-50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(5)
-    elif ((x>50 and x<150)and(y<50 and y>-50)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(6)
-    elif ((x>-150 and x<-50)and(y<-50 and y>-150)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(7)
-    elif ((x>-50 and x<50)and(y<-50 and y>-150)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(8)
-    elif ((x>50 and x<150)and(y<-50 and y>-150)):
-        notify.clear()
-        notify.write('',move=False, align="center", font=("Arial", 18, "normal"))
-        draw(9)
-    else:
-        notify.write('Please Select Valid',move=False, align="center", font=("Arial", 18, "normal"))
-bg()#to create BACKGROUNG
-initalizer()#starting game
+
+class Tile(Turtle):
+    def __init__(self, game, row, col):
+        super().__init__(shape="square")
+        self.game=game
+        self.row= row
+        self.col=col
+        self.color_value="black"
+        self.shapesize(5,5,None)#1 unit of length is = 20 pixls
+        x, y= self.coords(row, col)
+        self.up()
+        self.goto(x, y)
+    def change_color(self, x, y):
+        #x, y are postion of clicked but now not requried
+        if self.game.player==0:
+            if (self.xcor(),self.ycor())not in self.game.over:
+                self.color("red")
+                
+                self.game.player+=1
+                self.color_value="red" #we denoting red color by text
+                self.verify()
+        else:
+            self.color("blue")
+            self.game.player=0
+            self.color_value="blue"
+            self.verify()
+        self.game.over.append((lambda : (self.xcor()+110)/110,lambda : (110-self.ycor())/110))
+    def coords(self, row, col):
+        x=(-110+row*110)
+        y=(110-col*110)
+        return x,y
+    def verify(self):
+        
+        cheker_lst=[] 
+        C_N=0#constant
+        for tiles in self.game.tile.values():
+            cheker_lst.append(tiles.color_value)
+        for i in range(3):
+            if (cheker_lst[C_N]==cheker_lst[C_N+1])and(cheker_lst[C_N+1]==cheker_lst[C_N+2])and(cheker_lst[C_N]!="black"):
+                self.game.control.writer.write("{} is the winner".format(cheker_lst[C_N]),move=False,font=('Arial',18,"bold"))
+                
+            C_N+=3
+        C_N=0
+        for i in range(3):
+            if (cheker_lst[C_N]==cheker_lst[C_N+3])and(cheker_lst[C_N+3]==cheker_lst[C_N+6])and(cheker_lst[C_N]!="black"):
+                self.game.control.writer.write("{} is the winner".format(cheker_lst[C_N]),move=False,font=('Arial',18,"bold"),align="center")
+            C_N+=1
+        C_N=0
+        
+        if (cheker_lst[C_N]==cheker_lst[C_N+4])and(cheker_lst[C_N+4]==cheker_lst[C_N+8])and(cheker_lst[C_N]!="black"):
+            self.game.control.writer.write("{} is the winner".format(cheker_lst[C_N]),move=False,font=('Arial',18,"bold"),align="center")
+        C_N+=2
+        if (cheker_lst[C_N]==cheker_lst[C_N+2])and(cheker_lst[C_N+2]==cheker_lst[C_N+4])and(cheker_lst[C_N]!="black"):
+            self.game.control.writer.write("{} is the winner".format(cheker_lst[C_N]),move=False,font=('Arial',18,"bold"),align="center")
+        
+        cheker_lst=[]
+
+
+class controller:
+    def __init__(self,game):
+        self.game=game
+        self.notice()
+        self.setup()
+    def notice(self):
+        self.writer=Turtle(visible=False)
+        self.writer.pu()
+        self.writer.goto(0,-200)
+        
+    def setup(self):
+        for tiles in self.game.tile.values():
+            tiles.onclick(tiles.change_color,1)
+class main:
+    def __init__(self):
+        self.tile={}
+        self.player=0
+        self.created=[]
+        self.over=[]
+        self.setup()
+        self.control=controller(self)
+    def setup(self):
+        for row in range(3):
+            for col in range(3):
+                self.tile[(row,col)]=Tile(self, row, col)#aligning tiles in its position
+                self.created.append((row,col))
+
+win=Screen()
+win.screensize(680,560)
+win.colormode(255)#changing turtle screen to accept RBG color code.
+win.bgcolor((52,152,219))
+main()
+
 win.mainloop()
---
+++ pac-man - v2.py
@@ -0,0 +1,336 @@
+from turtle import Screen, Turtle
+import random,time
+FONT = ('Arial', 18, 'bold')
+runs=False
+cruns=False
+notout=True
+def computermove(game):
+    for i in game.enemy:
+        normal_move_logic(game.enemy[i], game)
+
+
+def normal_move_logic(enemy_in, game_in):
+    rn = 0
+    if enemy_in.last_dir is not None and enemy_in.last_key is not None:
+        if enemy_in.last_key in game_in.wall:
+            random.seed(time.time())
+            rn = random.randint(0, 3)
+        else:
+            rn = enemy_in.last_key
+    else:
+        random.seed(time.time())
+        rn = random.randint(0, 3)
+    if rn == 0:
+        enemy_in.go_up(game_in)
+    elif rn == 1:
+        enemy_in.go_down(game_in)
+    elif rn == 2:
+        enemy_in.go_left(game_in)
+    else:
+        enemy_in.go_right(game_in)   
+class Creator(Turtle):
+    def __init__(self, row, col, sprite, colorcode, width, height, game):
+        self.game=game
+        super().__init__(shape=sprite)
+        self.row = row
+        self.col = col
+        
+        self.color(colorcode)
+        self.shapesize(width, height, 3)
+        self.penup()
+        self.speed('fastest')
+        self.goto(self.coords(row, col))
+    @staticmethod
+    def coords(row, col):
+        x = col * 25 - 250
+        y = 137.5 - row * 25
+
+        return x, y
+    
+    @staticmethod
+    def inv_coords(x, y):
+        col = round((x + 250) / 25)
+        row = round((137.5 - y) / 25)
+
+        return row, col
+
+    def go_left1(self):
+        global runs
+        if runs==False:
+            runs=True
+            position = self.xcor() - 25, self.ycor()
+
+            key = self.inv_coords(*position)
+
+            if key not in self.game.wall:
+
+                if ((key in self.game.food)and(self.game.food[key].isvisible()==True)):
+                    self.game.food[key].hideturtle()
+                    self.game.score += 1
+                    self.game.running.writer.clear()
+                    self.game.running.writer.write("score:{}".format(self.game.score), font=FONT)
+                    if self.game.score==102:
+                        screen.clear()
+                        self.game.running.writer.clear()
+                        self.game.running.writer.goto(0,0)
+                        slef.game.running.writer.write('game over')
+                self.goto(self.coords(*key))
+            runs=False
+
+
+    def go_right1(self):
+        global runs
+        if runs==False:
+            runs=True
+
+            position = self.xcor() + 25, self.ycor()
+
+            key = self.inv_coords(*position)
+
+            if key not in self.game.wall:
+
+                if ((key in self.game.food)and(self.game.food[key].isvisible()==True)):
+                    self.game.food[key].hideturtle()
+                    self.game.score += 1
+                    self.game.running.writer.clear()
+                    self.game.running.writer.write("score:{}".format(self.game.score), font=FONT)
+                    if self.game.score==102:
+                        screen.clear()
+                        self.game.running.writer.clear()
+                        self.game.running.writer.goto(0,0)
+                        self.game.running.writer.write('game over')
+
+                self.goto(self.coords(*key))
+
+            runs=False
+    def go_up1(self):
+        global runs
+        if runs==False:
+            runs=True
+
+            position = self.xcor(), self.ycor() + 25
+
+            key = self.inv_coords(*position)
+
+            if key not in self.game.wall:
+
+                if ((key in self.game.food)and(self.game.food[key].isvisible()==True)):
+                    self.game.food[key].hideturtle()
+                    self.game.score += 1
+                    self.game.running.writer.clear()
+                    self.game.running.writer.write("score:{}".format(self.game.score), font=FONT)
+                    if self.game.score==102:
+                        screen.clear()
+                        self.game.running.writer.clear()
+                        self.game.running.writer.goto(0,0)
+                        self.game.running.writer.write('game over')
+        
+                self.goto(self.coords(*key))
+            runs=False
+
+    def go_down1(self):
+        global runs
+        if runs==False:
+            runs=True
+
+            position = self.xcor(), self.ycor() - 25
+
+            key = self.inv_coords(*position)
+
+            if key not in self.game.wall:
+
+                if ((key in self.game.food)and(self.game.food[key].isvisible()==True)):
+                    self.game.food[key].hideturtle()
+                    self.game.score += 1
+                    self.game.running.writer.clear()
+                    self.game.running.writer.write("score:{}".format(self.game.score), font=FONT)
+                    if self.game.score==102:
+                        screen.clear()
+                        self.game.running.writer.clear()
+                        self.game.running.writer.goto(0,0)
+                        self.game.running.writer.write('game over')
+
+                self.goto(self.coords(*key))
+            runs=False
+
+class Enemy_creator(Turtle):
+    def __init__(self, row, col, sprite, colorcode, width, height):
+        super().__init__(shape=sprite)
+        self.row = row
+        self.col = col
+
+        self.color(colorcode)
+        self.shapesize(width, height, 3)
+        self.penup()
+        self.speed('fastest')
+        self.goto(self.coords(row, col))
+
+        self.last_key = None
+        self.last_dir = None
+
+    @staticmethod
+    def coords(row, col):
+        x = col * 25 - 250
+        y = 137.5 - row * 25
+
+        return x, y
+
+    @staticmethod
+    def inv_coords(x, y):
+        col = round((x + 250) / 25)
+        row = round((137.5 - y) / 25)
+
+        return row, col
+
+    def go_left(self, game):
+        global cruns, notout
+        if cruns == False:
+            cruns = True
+            self.game = game
+            position = self.xcor() - 25, self.ycor()
+
+            key = self.inv_coords(*position)
+            if position == (game.player[9, 9].xcor(), game.player[9, 9].ycor()):
+                screen.clear()
+                notout = False
+            if key not in game.wall:
+                self.setheading(0)
+                self.goto(self.coords(*key))
+            self.last_key = key
+            self.last_dir = 2
+
+            cruns = False
+
+    def go_right(self, game):
+        global cruns, notout
+
+        if cruns == False:
+            cruns = True
+            self.game = game
+
+            position = self.xcor() + 25, self.ycor()
+
+            key = self.inv_coords(*position)
+            if position == (game.player[9, 9].xcor(), game.player[9, 9].ycor()):
+                screen.clear()
+                notout = False
+
+            if key not in game.wall:
+                self.setheading(180)
+                self.goto(self.coords(*key))
+
+            self.last_key = key
+            self.last_dir = 3
+            cruns = False
+
+    def go_up(self, game):
+        global cruns, notout
+
+        if cruns == False:
+            cruns = True
+            self.game = game
+
+            position = self.xcor(), self.ycor() + 25
+
+            key = self.inv_coords(*position)
+            if position == (game.player[9, 9].xcor(), game.player[9, 9].ycor()):
+                screen.clear()
+                notout = False
+
+            if key not in game.wall:
+                self.setheading(90)
+                self.goto(self.coords(*key))
+
+            self.last_key = key
+            self.last_dir = 0
+            cruns = False
+
+    def go_down(self, game):
+        global cruns, notout
+
+        if cruns == False:
+            cruns = True
+            self.game = game
+
+            position = self.xcor(), self.ycor() - 25
+
+            key = self.inv_coords(*position)
+            if position == (game.player[9, 9].xcor(), game.player[9, 9].ycor()):
+                screen.clear()
+                notout = False
+
+            if key not in game.wall:
+                self.setheading(-90)
+                self.goto(self.coords(*key))
+            self.last_key = key
+            self.last_dir = 1
+            cruns = False
+class Running:
+    def __init__(self, game):
+        self.game = game
+
+        self.writer = Turtle(visible=False)
+        self.writer.penup()
+        self.writer.color('blue')
+        self.writer.goto(200, 170)
+        self.writer.write("score:{}".format(game.score), font=FONT)
+
+        screen.onkey(game.player[9, 9].go_left1, 'a')
+        screen.onkey(game.player[9, 9].go_right1, 'd')
+        screen.onkey(game.player[9, 9].go_up1, 'w')
+        screen.onkey(game.player[9, 9].go_down1, 's')
+        screen.onkey(self.movingcomputer,'space')
+        screen.onkey(self.exit,'q')
+        screen.listen()
+    def exit(self):
+        global notout
+        notout=False
+        exit()
+    def movingcomputer(self):
+        while notout:
+            computermove(self.game)
+
+class Pacman:
+    def __init__(self):
+        self.wall = {}
+        self.player = {}
+        self.food = {}
+        self.enemy = {}
+
+        self.score = 0
+        screen.tracer(0)
+        self.setup()
+        screen.tracer(1)
+        self.running = Running(self)
+    def setup(self):
+        for row in range(11):
+            for col in range(20):
+                if level[row][col] == 1:
+                    self.wall[(row, col)] = Creator(row, col, 'square', 'blue', 1, 1, self)
+                elif level[row][col] == 2:
+                    color=random.choice(['red','green','skyblue','gray'])
+                    self.enemy[(row, col)] = Enemy_creator(row, col, 'triangle', color, 1, 1)
+                elif level[row][col] == 3:
+                    self.player[(row, col)] = Creator(row, col, 'circle', 'yellow', 1, 1, self)
+                else:
+                    self.food[(row, col)] = Creator(row, col, 'circle', 'white', 0.1, 0.1, self)
+
+
+level = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 2, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+screen = Screen()
+screen.bgcolor('black')
+game = Pacman()
+screen.mainloop()
